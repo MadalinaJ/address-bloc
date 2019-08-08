d7e23d320c7f8aff436636a34b55db0b98d9ab4f
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-08 08:56:44 -0700

    add choice

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,7 +9,9 @@ const inquirer = require('inquirer');
           message: "Please choose from an option below: ",
           choices: [
             "Add new contact",
+            "Get date"
             "Exit"
+
           ]
         }
       ];
