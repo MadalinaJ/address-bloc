2e73820f10c9b8dcfaa92eb927a39af80e8ff31e
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-10 23:59:11 -0700

    assignment-contacts-database

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -19,10 +19,11 @@ describe("ContactController", () => {
     describe("#addContact()", () => {
  
       it("should add a single contact into the book", (done) => {
-        this.book.addContact("Alice", "001-101-1010")
+        this.book.addContact("Alice", "001-101-1010", "alice@bee.com")
         .then((contact) => {
-            expect(contact.name).toBe("Alice");
+          expect(contact.name).toBe("Alice");
           expect(contact.phone).toBe("001-101-1010");
+          expect(contact.email).toBe("alice@bee.com");
           done();
         })
         .catch((err) => {
